8340d1cc4bb5a992a9809c86950d1709b49e9da4
sally <wykee001@gmail.com>
2024-09-11 16:49:23 -0400

    Commit1

---
+++ lab01/part1_modules/lab1.js
@@ -0,0 +1,20 @@
+const _ = require("lodash");
+
+const holidays = [
+    {name: "Christmas", date: new Date("2024-12-25")},
+    {name: "Canada Day", date: new Date("2025-07-01")},
+    {name: "New Year", date: new Date("2025-01-01")}
+]
+
+const today = new Date();
+
+holidays.forEach(holiday =>{
+    console.log(holiday)
+    console.log((holiday.date - today)/(1000*60*60*24))
+})
+
+const randomHoliday = _.sample(holidays);
+console.log(randomHoliday)
+
+const indexHoliday = _.findIndex(holidays, {name: "Christmas"});
+console.log(indexHoliday)
