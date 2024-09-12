0e1a93019cd6f790a6be1fa6d87bb96de8787d77
sally <wykee001@gmail.com>
2024-09-12 13:14:46 -0400

    commit3

--- lab01/part2_bookstore/server.js
+++ lab01/part2_bookstore/server.js
@@ -1,76 +1,77 @@
 const http = require("http")
-const PORT = 8000 || process.env.PORT;
 const fs = require("fs")
 const path = require("path")
- 
+const PORT = process.env.PORT || 8000;
  
 const server = http.createServer((req, res) => {
+
     if(req.url === "/"){
         fs.readFile(path.join(__dirname, "pages", "home.html"), "utf8", (err, data) => {
             if (err) {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.write("Server Error");
                 res.end();
-                throw err;
-                
             }
-            res.write(data)
-            res.end()
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            res.end();
           });
     }
     else if(req.url === "/about"){
-        res.write("Hello to the about")
-        res.end()
+        fs.readFile(path.join(__dirname, "pages", "about.html"), "utf8", (err, data) => {
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.write("Server Error");
+                res.end();
+            }
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            res.end();
+          });
     }
     else if(req.url === "/contact"){
-        res.write("Hello to the contact")
-        res.end()
+        fs.readFile(path.join(__dirname, "pages", "contact.html"), "utf8", (err, data) => {
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.write("Server Error");
+                res.end();
+            }
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            res.end();
+          });
     }
     else if (req.url === "/login"){
         if(req.method === "POST") {
-            res.end("GOT THE DATA")
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("GOT THE DATA");
         }
-        res.write(`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <header>
-        <nav>
-            <a href="home.html">Home</a>
-            <a href="about.html">About</a>
-            <a href="contact.html">Contact</a>
-        </nav>
-    </header>
-    <main>
-        <h1>Login</h1>
-        <form action="/login" method="POST">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required><br><br>
- 
- 
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
- 
- 
-            <input type="submit" value="Login">
-        </form>
-        <p>Don't have an account? <a href="signup.html">Sign Up</a></p>
-    </main>
-</body>
-</html>
-`)
-        res.end()
+        fs.readFile(path.join(__dirname, "pages", "login.html"), "utf8", (err, data) => {
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.write("Server Error");
+                res.end();
+            }
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            res.end();
+    });
+    } else {
+        fs.readFile(path.join(__dirname, "pages", "404.html"), "utf8", (err, data) => {
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.write("Server Error");
+                res.end();
+            }
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            res.end();
+
     }
-    else {
-        res.write("Page not found")
-        res.end()
-    }
-  });
+  )};
  
  
-  server.listen(PORT, ()=>{
-    console.log(`http://localhost:${PORT}`)
-  })
+  server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+});
