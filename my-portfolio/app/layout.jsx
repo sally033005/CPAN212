b865f94bee9b86c4c11ba9fdb97086aa69ef1980
sally <wykee001@gmail.com>
2025-08-21 14:48:29 -0400

    path corrected in layout.jsx

--- my-portfolio/app/layout.jsx
+++ my-portfolio/app/layout.jsx
@@ -1,6 +1,6 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 
